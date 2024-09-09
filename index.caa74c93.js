c55e526c9721028cf35dcb917caec50e743be1eb
Kamil <kamilbykowski99@gmail.com>
2024-09-09 15:33:20 +0200

    make build

---
+++ index.caa74c93.js
@@ -0,0 +1,2 @@
+var t=document.querySelector("#largeImg"),e=document.querySelectorAll(".list-item__link"),r=Array.from(e);r.forEach(function(t){t.getAttribute("href")}),e.forEach(function(e,c){e.addEventListener("click",function(e){e.preventDefault(),t.setAttribute("src","".concat(r[c]))})});
+//# sourceMappingURL=index.caa74c93.js.map
